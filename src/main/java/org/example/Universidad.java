a4af5fa3067b91ee8e4455cf2eb1dfd13b1321f8
DavidAikoo 👑 <zmateoh1998@gmail.com>
2026-02-11 11:11:45 -0500

    Update Universidad.java

--- src/main/java/org/example/Universidad.java
+++ src/main/java/org/example/Universidad.java
@@ -3,12 +3,34 @@ package org.example;
 import java.util.ArrayList;
 
 class Universidad {
+
     private String nombre;
     private ArrayList<Estudiante> estudiantes;
+
+    public Universidad() {
+        this.estudiantes = new ArrayList<>();
+    }
 
     public Universidad(String nombre) {
         this.nombre = nombre;
         this.estudiantes = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEstudiantes(ArrayList<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
     }
 
     public void agregarEstudiante(Estudiante estudiante) {
@@ -16,9 +38,19 @@ class Universidad {
     }
 
     public void mostrarEstudiantes() {
-        System.out.println("Universidad: " + nombre);
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Universidad: ").append(nombre).append("\n");
+        sb.append("Estudiantes:\n");
+
         for (Estudiante e : estudiantes) {
-            System.out.println("- " + e.getNombre());
+            sb.append("- ").append(e.getNombre()).append("\n");
         }
+
+        return sb.toString();
     }
 }
