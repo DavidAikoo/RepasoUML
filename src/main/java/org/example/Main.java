f3055dcdbc4d4036f45207e05fc0501d151b64ea
DavidAikoo 👑 <zmateoh1998@gmail.com>
2026-02-11 11:12:04 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,14 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        // CREAR ESTUDIANTES
         Estudiante e1 = new Estudiante("Carlos");
         Estudiante e2 = new Estudiante("María");
 
-        // CREAR UNIVERSIDAD
-        Universidad u = new Universidad("Universidad Nacional");
+        Universidad u = new Universidad("Universidad de Cundinamarca");
 
-        // AGREGAR ESTUDIANTES
         u.agregarEstudiante(e1);
         u.agregarEstudiante(e2);
 
